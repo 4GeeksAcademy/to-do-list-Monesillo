39b757bf95d7f23f85be0f3f1a91d2bc9f231c4a
Monesillo <162189011+Monesillo@users.noreply.github.com>
2024-08-15 05:21:22 +0000

    proyecto finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo]);
+      setNewTodo("");
+    }
+  };
 
-export default Home;
+  const handleDelete = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="todo-container">
+      <h1 className="todo-title">todos</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="What needs to be done?"
+          className="todo-input"
+        />
+      </form>
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index} className="todo-item">
+            {todo}
+            <button
+              className="delete-button"
+              onClick={() => handleDelete(index)}
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="todo-footer">
+        <span>{todos.length} tarea{todos.length !== 1 && 's'} restante{todos.length !== 1 && 's'}</span>
+      </div>
+    </div>
+  );
+}
+
+export default TodoList;
